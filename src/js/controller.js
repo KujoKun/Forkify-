209f7898ac1845555d615c118164ddb8f84b5323
KujoKun <kazuya.kujo2@gmail.com>
2021-11-05 22:24:21 +0000

    Inital commit

---
+++ src/js/controller.js
@@ -0,0 +1,47 @@
+// import 'regenerator-runtime/runtime';
+import * as model from './model.js';
+import recipeView from './views/recipeViews.js';
+import searchView from './views/searchView';
+
+const timeout = function (s) {
+  return new Promise(function (_, reject) {
+    setTimeout(function () {
+      reject(new Error(`Request took too long! Timeout after ${s} second`));
+    }, s * 1000);
+  });
+};
+//11364f33-fa03-454b-a0cf-3a000102b9fd
+const controlRecipe = async function () {
+  try {
+    const id = window.location.hash.slice(1);
+    if (!id) return;
+    recipeView.renderSpinner();
+    //Loading recipe
+
+    await model.loadRecipe(id);
+    // console.log(recipe);
+
+    //2: Rendering recipe
+    recipeView.render(model.state.recipe);
+  } catch (err) {
+    recipeView.renderError();
+    // console.log(err);
+  }
+};
+
+const controlSearchResult = async function () {
+  try {
+    const query = searchView.getQuery();
+    if (!query) return;
+    await model.loadSearchResult(query);
+    console.log(model.state.search.results);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+const init = function () {
+  recipeView.addHandlerRender(controlRecipe);
+  searchView.addHandlerSearch(controlSearchResult);
+};
+init();
